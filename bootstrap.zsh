c2beaf9c99f3a4017621d4106c62594609bc3351
Lukacs Zoltan <zozz@troll-net.hu>
2015-03-27 23:52:09 +0100

    Add prezto rep clone to bootstrap script

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -9,6 +9,38 @@ pushd $(dirname $0) >/dev/null
 script_path=$(pwd -P)
 popd >/dev/null
 
+# Path of the Oh-my-zsh repo
+if [ ! -n "${ZSH}" ]; then
+    ZSH="${ZDOTDIR:-$HOME}/.zprezto"
+fi
+
+
+function installPrezto() {
+
+    echo -n "Installing Prezto... "
+
+    # Clone the Prezto git repo
+    local prezto_repo="https://github.com/sorin-ionescu/prezto.git"
+    hash git >/dev/null && /usr/bin/env git clone --recursive "${prezto_repo}" "${ZSH}" || {
+        echo "✗"
+        echo "Git not installed"
+        exit 1
+    }
+    echo "✔"
+   
+    # Setup Prezto
+    setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+       ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
+   
+    chsh -s /bin/zsh
+       
+    if [[ ! $? -eq 0 ]]; then echo "✗"
+    else echo "✔"
+    fi
+}
+
 function doIt() {
     cd "${script_path}"
 
@@ -25,23 +57,13 @@ function doIt() {
     echo -n "Syncing files... "
 
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.zsh" \
-        --exclude "README.md" -avq --no-perms . ${HOME}
+        --exclude "README.md" -avq --no-perms . ${ZDOTDIR:-$HOME}
 
     if [[ ! $? -eq 0 ]]; then echo "✗"
     else echo "✔"
     fi
 
-    # Setup Prezto
-    setopt EXTENDED_GLOB
-    for rcfile in "${$HOME}"/.zprezto/runcoms/^README.md(.N); do
-      ln -s "$rcfile" "${$HOME}/.${rcfile:t}"
-    done
-    
-    chsh -s /bin/zsh
-    
-    if [[ ! $? -eq 0 ]]; then echo "✗"
-    else echo "✔"
-    fi
+   
 }
 
 
